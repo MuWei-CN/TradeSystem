ebe7875c58aa1d118d6f7d1acc6381a5b12410ef
MuWei-CN <137994544+MuWei-CN@users.noreply.github.com>
2025-03-23 11:01:25 +0800

    更新项目结构

--- src/main/java/com/bistu/exr/service/SInterface/CommonUserService.java
+++
@@ -1,9 +0,0 @@
-package com.bistu.exr.service.SInterface;
-
-import com.bistu.exr.dao.model.CommonUser;
-
-// CommonUserService接口，定义了获取用户信息的方法
-public interface CommonUserService {
-    public CommonUser getUserByName(String name);
-
-}
